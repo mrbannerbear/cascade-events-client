fa956e52a3904416134de037843fae40e7b42fa4
S M Mishkatus Saqlain <smmsaqlain@gmail.com>
2024-01-31 19:16:38 +0600

    projects test2

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,7 +1,29 @@
 const Projects = () => {
+    const resources = [
+        {
+            img: "/projects/1.jpg"
+        },
+        {
+            img: "/projects/2.jpg"
+        },
+        {
+            img: "/projects/3.jpg"
+        }
+    ]
     return (
         <div className="min-h-screen">
-            <img src="/projects/1.jpg" alt="" />
+            <div className="grid grid-cols-2 md:grid-cols-3">
+            {
+                resources.map(
+                    each => (
+                        <button>
+                             <img src={each.img} className="h-40" alt="" />
+                        </button>
+                )
+                )
+            }
+            </div>
+           
         </div>
     );
 };
