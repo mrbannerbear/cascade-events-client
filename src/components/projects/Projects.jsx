3d66458be3610ba21a067b48ef23d8f3242d375c
S M Mishkatus Saqlain <smmsaqlain@gmail.com>
2024-03-06 20:52:21 +0600

    Added minor fixes

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -2,18 +2,18 @@ import Banner from "../home/homeComps/Banner";
 
 const Projects = () => {
   const resources = [
-    { id: 1, img: "/projects/1.jpg" },
-    { id: 2, img: "/projects/2.jpg" },
-    { id: 3, img: "/projects/3.jpg" },
-    { id: 4, img: "/projects/4.jpg" },
-    { id: 5, img: "/projects/5.jpg" },
-    { id: 6, img: "/projects/6.jpg" },
-    { id: 7, img: "/projects/7.jpg" },
-    { id: 8, img: "/projects/8.jpg" },
-    { id: 9, img: "/projects/9.jpg" },
-    { id: 10, img: "/projects/10.jpg" },
-    { id: 11, img: "/projects/11.jpg" },
-    { id: 12, img: "/projects/12.jpg" },
+    { id: 1, img: "/projects/1.webp" },
+    { id: 2, img: "/projects/2.webp" },
+    { id: 3, img: "/projects/3.webp" },
+    { id: 4, img: "/projects/4.webp" },
+    { id: 5, img: "/projects/5.webp" },
+    { id: 6, img: "/projects/6.webp" },
+    { id: 7, img: "/projects/7.webp" },
+    { id: 8, img: "/projects/8.webp" },
+    { id: 9, img: "/projects/9.webp" },
+    { id: 10, img: "/projects/10.webp" },
+    { id: 11, img: "/projects/11.webp" },
+    { id: 12, img: "/projects/12.webp" },
   ];
   return (
     <div className="min-h-screen">
